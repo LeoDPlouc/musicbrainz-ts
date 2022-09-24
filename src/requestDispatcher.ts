ee355a96f0d1ddad2fb83f79c67dca9c5f59bb76
LeoDplouc <LeoDplouc>
2022-09-24 13:32:55 +0200

    Added request dispatcher to respect MusibBrainz Api rate limit

---
+++ src/requestDispatcher.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+
+type ResponseType = AxiosResponse<any, any>
+
+let interval: number | undefined = undefined
+let queue: (() => Promise<void>)[] = []
+
+function startTimer() {
+    if (!interval) {
+        interval = setInterval(dispatchRequest, 1000)
+    }
+}
+
+function clearTimer() {
+    if (!queue.length) {
+        clearInterval(interval)
+        interval = undefined
+    }
+}
+
+function dispatchRequest() {
+    let requestGenerator = queue.pop()
+    if (requestGenerator) {
+        requestGenerator()
+    }
+    clearTimer()
+}
+
+export function queueRequest(url: string, config?: AxiosRequestConfig<any> | undefined): Promise<ResponseType> {
+    return new Promise<ResponseType>((resolve, reject) => {
+        queue.push(() => axios.get(url, config).then(res => resolve(res)).catch(err => reject(err)))
+        startTimer()
+    })
+}
--- src/requests.ts
+++ src/requests.ts
@@ -1,10 +1,12 @@
 import axios from "axios"
+import { queueRequest } from "./requestDispatcher"
 
 let apiUrl = "https://musicbrainz.org/ws/2/"
 
 export async function searchRecordingRequest(query: string, useragent: string): Promise<RecordingSearchResults> {
     let url = encodeURI(`${apiUrl}recording?query=${query}`)
-    const res = await axios.get(url, { headers: { useragent: useragent, accept: "application/json" } })
+
+    const res = await queueRequest(url, { headers: { useragent: useragent, accept: "application/json" } })
 
     return res.data
 }
